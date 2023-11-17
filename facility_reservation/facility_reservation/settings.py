d2de16ef663d85045c8e7a915dd4a6f286e6bee3
spearm1ntx <francisbendulo@gmail.com>
2023-11-17 19:11:28 +0800

    first commit

--- facility_reservation/facility_reservation/settings.py
+++ facility_reservation/facility_reservation/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 import os
 
@@ -30,6 +31,8 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+CORS_ORIGIN_ALLOW_ALL=True
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -39,10 +42,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'wallet',
+    'api',
     'walletAPI',
     'rest_framework',
     'rest_framework.authtoken',
-    
+    'wiladmin',
+    'polls'
 ]
 
 
@@ -86,7 +91,7 @@ WSGI_APPLICATION = 'facility_reservation.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'wallet_db',
+        'NAME': 'wilms',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': '127.0.0.1',
@@ -158,3 +163,62 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'wilms.wallet@gmail.com'
 EMAIL_HOST_PASSWORD = 'vbbh nvxm vtcp yois'    
+
+# settings pra sa reservations
+SECRET_KEY = 'django-insecure-yx0_es9okr6pcsa_xd-9@u3##kymq@_hga+aj1whai*9q%zsxt'
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": False,
+    "UPDATE_LAST_LOGIN": False,
+
+    "ALGORITHM": "HS256",
+    "SIGNING_KEY": SECRET_KEY,
+    "VERIFYING_KEY": "",
+    "AUDIENCE": None,
+    "ISSUER": None,
+    "JSON_ENCODER": None,
+    "JWK_URL": None,
+    "LEEWAY": 0,
+
+    "AUTH_HEADER_TYPES": ("Bearer",),
+    "AUTH_HEADER_NAME": "HTTP_AUTHORIZATION",
+    "USER_ID_FIELD": "id",
+    "USER_ID_CLAIM": "user_id",
+    "USER_AUTHENTICATION_RULE": "rest_framework_simplejwt.authentication.default_user_authentication_rule",
+
+    "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
+    "TOKEN_TYPE_CLAIM": "token_type",
+    "TOKEN_USER_CLASS": "rest_framework_simplejwt.models.TokenUser",
+
+    "JTI_CLAIM": "jti",
+
+    "SLIDING_TOKEN_REFRESH_EXP_CLAIM": "refresh_exp",
+    "SLIDING_TOKEN_LIFETIME": timedelta(minutes=5),
+    "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
+
+    "TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainPairSerializer",
+    "TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSerializer",
+    "TOKEN_VERIFY_SERIALIZER": "rest_framework_simplejwt.serializers.TokenVerifySerializer",
+    "TOKEN_BLACKLIST_SERIALIZER": "rest_framework_simplejwt.serializers.TokenBlacklistSerializer",
+    "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
+    "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
+}
+
+REST_FRAMEWORK = {
+
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+
+        'rest_framework_simplejwt.authentication.JWTTokenUserAuthentication',
+    )
+
+}
+
+# settings for walkin
+LOGIN_URL = 'user_login'
+LOGIN_REDIRECT_URL = 'user_dashboard'
+
+
+SECURE_CROSS_ORIGIN_OPENER_POLICY = None
--- facility_reservation/facility_reservation/urls.py
+++ facility_reservation/facility_reservation/urls.py
@@ -1,17 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
 from wallet import views
-
+from api import urls
 from django.contrib.auth import views as auth_views
-
+from wiladmin import urls
+from polls import urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('wallet/', include('wallet.urls', namespace='wallet')),  # Include app-specific URLs with the 'wallet' namespace
     path('walletAPI/', include(('walletAPI.urls', 'walletAPI'), namespace='walletAPI')),
     path('' , views.IndexView.as_view(), name='index'),
-    
-    
-    
+    path('api/',include('api.urls')),
+    path('',include('polls.urls',)),
+    path('',include('wiladmin.urls',)),
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='wallet/password_reset.html'), name="password_reset"),
 
     path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='wallet/password_reset_done.html'), name = "password_reset_done"),
